805244a4cc33c43edf832b9c81dd8f79e5b71f92
Ropzt <parizot.ro@gmail.com>
2021-09-23 00:16:03 +0200

    Update Sphere.cs

--- ModelisationGeom/Sphere.cs
+++ ModelisationGeom/Sphere.cs
@@ -13,7 +13,7 @@ public class Sphere : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float ecartParalleles = hauteur/nb_paralleles;
+        float ecartParalleles = (float)hauteur/(float)nb_paralleles;
         int nb_vertices = (nb_meridiens*nb_paralleles)+nb_paralleles;
         Vector3 centre = new Vector3(0f,0f,0f);
 
@@ -26,7 +26,7 @@ public class Sphere : MonoBehaviour
 
         int indexVertices = 0;
 
-        for(int j=0; j<nb_paralleles; j++)
+        for(int j=1; j<nb_paralleles+1; j++)
         {
             for( int i=0; i<nb_meridiens; i++)
             {
@@ -38,21 +38,26 @@ public class Sphere : MonoBehaviour
                 float y = rayon*Mathf.Sin(angle)*Mathf.Sin(phi);
                 float z = rayon*Mathf.Cos(phi);
 
-                Debug.Log("X = "+x+" , Y = "+y+" , Z = "+z);
+                //Debug.Log("Parallele "+j+", Point "+indexVertices+", X = "+x+" , Y = "+y+" , Z = "+z);
 
                 vertices[indexVertices]=new Vector3(x,y,z);
                 indexVertices++;
             }
         }
-        Debug.Log("Index Vertice ="+indexVertices);
 
-        for(int j=1; j<nb_paralleles; j++)
+        //Ecriture des centres des cercles
+        for(int j=1; j<nb_paralleles+1; j++)
         {
-            //Gestion des centres de chaque paralleles (les mettres à la fin)
-            vertices[(nb_vertices-nb_paralleles)+j] = new Vector3(0f,0f, (0f-(hauteur/2)+(ecartParalleles*j)));
+            float phi = ((Mathf.PI*j)/nb_paralleles);
+            vertices[indexVertices+j-1] = new Vector3(0f,0f, (rayon*Mathf.Cos(phi)));
+            
+            //Debug.Log("Point "+(indexVertices+j-1)+", X = "+0+" , Y = "+0+" , Z = "+(rayon*Mathf.Cos(phi)));
         }
 
+        //Ecriture des poles nord et sud
+        // A FAIRE
 
+        //Ecriture des triangles pour faire apparaitre les cercles
         List<int> triangle = new List<int>();
 
         for(int j = 0; j < nb_paralleles ; j++)
@@ -60,15 +65,50 @@ public class Sphere : MonoBehaviour
             for(int i=0;i<nb_meridiens;i++)
             {
                 triangle.Add((nb_vertices-nb_paralleles)+j);
-                triangle.Add(((j*nb_meridiens))+i);
-                triangle.Add(((j*nb_meridiens))+i+1);
+                triangle.Add((j*nb_meridiens)+i);
+                if((i+1) == nb_meridiens)
+                {
+                    triangle.Add((j*nb_meridiens));
+                }
+                else
+                {
+                    triangle.Add((j*nb_meridiens)+i+1);
+                }
 
-                Debug.Log("P1 = "+((nb_vertices-nb_paralleles)+j)+" , P2 = "+((j*nb_meridiens)+i)+" , Z = "+((j*nb_meridiens)+i+1));
+                //Debug.Log("P1 = "+((nb_vertices-nb_paralleles)+j)+" , P2 = "+((j*nb_meridiens)+i)+" , P3 = "+((j*nb_meridiens)+i+1));
             }
         }
 
+        //Ecriture des triangles pour faire apparaitre la sphere
+        List<int> outerTriangle = new List<int>();
+        
+        //Premier cercle vers pole 
+        // A FAIRE
+
+        //Cercles intermédiaires 
+        for(int j = 1; j < nb_paralleles-1 ; j++) //j=1 car skip 1st circle; nb_paralleles-1 car skip last circle
+        {
+            for(int i=0;i<nb_meridiens;i++)
+            {
+                outerTriangle.Add((j*nb_meridiens)+i);
+                outerTriangle.Add((j*nb_meridiens)+i+1);
+                outerTriangle.Add(((j+1)*nb_meridiens)+i+1);
+
+                outerTriangle.Add((j*nb_meridiens)+i);
+                outerTriangle.Add(((j+1)*nb_meridiens)+i+1);
+                outerTriangle.Add(((j+1)*nb_meridiens)+i);
+
+                Debug.Log("P1 = "+((j*nb_meridiens)+i)+" , P2 = "+((j*nb_meridiens)+i+1)+" , P3 = "+(((j+1)*nb_meridiens)+i+1));
+                Debug.Log("P1 = "+((j*nb_meridiens)+i)+" , P2 = "+(((j+1)*nb_meridiens)+i+1)+" , P3 = "+(((j+1)*nb_meridiens)+i));
+            }
+        }
+
+        //dernier cercle vers pole
+        //A FAIRE
+
         msh.vertices = vertices;
-        msh.triangles = triangle.ToArray();
+        //msh.triangles = triangle.ToArray();  //Si on veut faire apparaitre les cercles
+        msh.triangles = outerTriangle.ToArray();
         gameObject.GetComponent<MeshFilter>().mesh = msh;
         gameObject.GetComponent<MeshRenderer>().material = mat;
 
