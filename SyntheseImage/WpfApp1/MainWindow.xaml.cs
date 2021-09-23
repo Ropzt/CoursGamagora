a6eed3a00aac4007b2a75eaf4eb3d52150fcde17
Ropzt <parizot.ro@gmail.com>
2021-09-23 17:55:57 +0200

    Commit SyntheseImage Cours1 RayTracing Sphere

---
+++ SyntheseImage/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public class Ray
+    {
+        public Vector3D Origin;
+        public Vector3D Direction;
+
+        public Ray(Vector3D origin, Vector3D direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+    }
+
+    public class Sphere
+    {
+        public double Radius;
+        public Vector3D Center;
+
+        public Sphere(double radius, Vector3D center)
+        {
+            Radius = radius;
+            Center = center;
+        }
+
+        public bool Hit(Ray ray)
+        {
+            Vector3D co = ray.Origin - Center;
+            double b = Vector3D.DotProduct(ray.Direction, co);
+            double c = Vector3D.DotProduct(co, co) - Radius * Radius;
+            double disc = b * b - c;
+            return disc > 0;
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        WriteableBitmap bitmap;
+        byte[] pixels;
+        int width = 500;
+        int height = 500;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateImage();
+            Render();
+            SetImage();
+
+        }
+
+        void CreateImage()
+        {
+            bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Pbgra32, null);
+            pixels = new byte[(int)(width * height * 4)];
+        }
+
+        void Render()
+        {
+            Sphere sphere = new Sphere(2.0, new Vector3D(0, 0, -10));
+            Vector3D cameraPos = new Vector3D(0,0,5);
+            for(int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    int index =( h * width + w) *4;
+                    Vector3D pixPos = new Vector3D(w/255.0-1.0, h/255.0-1.0, 0);
+                    Vector3D dir = pixPos - cameraPos;
+                    dir.Normalize();
+
+                    Ray ray = new Ray(cameraPos, dir);
+
+                    if(sphere.Hit(ray))
+                    {
+                        pixels[index + 0] = 255;
+                        pixels[index + 1] = (byte)(w / 2);
+                        pixels[index + 2] = (byte)(h / 2);
+                        pixels[index + 3] = 255;
+                    }
+                    else
+                    {
+                        pixels[index + 0] = 0;
+                        pixels[index + 1] = 0;
+                        pixels[index + 2] = 0;
+                        pixels[index + 3] = 255;
+                    }
+                }
+            }
+        }
+
+        void SetImage()
+        {
+            int stride = width * 4;
+            bitmap.WritePixels(new Int32Rect(0, 0, width, height), pixels, stride,0,0);
+            image.Source = bitmap;
+        }
+    }
+}
