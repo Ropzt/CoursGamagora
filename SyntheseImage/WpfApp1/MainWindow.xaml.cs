ce08ef6b43dfe0e4f6c8c7c3d057dfe9687a9489
Ropzt <parizot.ro@gmail.com>
2021-09-27 20:54:31 +0200

    add getIntersectT and print min one

--- SyntheseImage/WpfApp1/MainWindow.xaml.cs
+++ SyntheseImage/WpfApp1/MainWindow.xaml.cs
@@ -43,13 +43,67 @@ namespace WpfApp1
             Center = center;
         }
 
-        public bool Hit(Ray ray)
+        public double getIntersectT(Ray ray)
         {
-            Vector3D co = ray.Origin - Center;
-            double b = Vector3D.DotProduct(ray.Direction, co);
+            
+            Vector3D co = ray.Origin - Center; //Get initial position
+            double b = Vector3D.DotProduct(ray.Direction, co); 
             double c = Vector3D.DotProduct(co, co) - Radius * Radius;
-            double disc = b * b - c;
-            return disc > 0;
+            double delta = b * b - c; //Get discriminant
+            if (delta < 0)
+            {
+                return 0;
+            }
+            double t1 = new double();
+            double dsquared = Math.Sqrt(delta);
+            if (delta == 0)
+            {
+                t1 = -b / 2;
+                if(t1<=0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return t1;
+                }
+            }
+            else
+            {
+                 t1 = -b + dsquared;
+                double t2 = -b - dsquared;
+                if(t1<=0 & t2<=0)
+                {
+                    return 0;
+                }
+                else if( t1>0)
+                {
+                    if(t2<=0)
+                    {
+                        return t1;
+                    }
+                    else if(t1==t2)
+                    {
+                        return t1;
+                    }
+                    else
+                    {
+                        double diff = t1 - t2;
+                        if(diff>0)
+                        {
+                            return t2;
+                        }
+                        else
+                        {
+                            return t1;
+                        }
+                    }
+                }
+                else
+                {
+                    return t2;
+                }
+            }
         }
     }
     public partial class MainWindow : Window
@@ -93,15 +147,13 @@ namespace WpfApp1
 
             List<Sphere> sphereList = new List<Sphere>();
             Sphere sphere1 = new Sphere(1.0, new Vector3D(0, 0, -10));
-            Sphere sphere2 = new Sphere(3.8, new Vector3D(2, 2, -20));
+            Sphere sphere2 = new Sphere(1.0, new Vector3D(1, 1, -9));
             sphereList.Add(sphere1);
             sphereList.Add(sphere2);
 
             Vector3D cameraPos = new Vector3D(0,0,5);
 
-            int sphereCount=0;
-            foreach(Sphere sphere in sphereList)
-            {
+            
                 for (int h = 0; h < height; h++)
                 {
                     for (int w = 0; w < width; w++)
@@ -112,20 +164,23 @@ namespace WpfApp1
                         dir.Normalize();
 
                         Ray ray = new Ray(cameraPos, dir);
-
-                        if (sphere.Hit(ray))
+                        int sphereCount = 0;
+                        double intersectMin = double.MaxValue;
+                        foreach (Sphere sphere in sphereList)
                         {
-                            pixels[index + 0] = 255;
-                            pixels[index + 1] = (byte)(50*sphereCount);
-                            pixels[index + 2] = (byte)(50*sphereCount);
-                            pixels[index + 3] = 255;
+                            double t = sphere.getIntersectT(ray);
+                            if (t !=0 & t < intersectMin)
+                            {
+                                intersectMin = t;
+                                pixels[index + 0] = 255;
+                                pixels[index + 1] = (byte)(50 * sphereCount);
+                                pixels[index + 2] = (byte)(50 * sphereCount);
+                                pixels[index + 3] = 255;
+                            }
+                            sphereCount++;
                         }
-                        
                     }
-                }
-                sphereCount++;
             }
-            
         }
 
         void SetImage()
